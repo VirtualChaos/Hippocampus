5244365cc472d8b82bf241faf2607c4298407d6d
VirtualChaos <VirtualChaos@users.noreply.github.com>
2021-12-15 00:28:25 +0800

    update mt scripts

--- mt/test.sh
+++ mt/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-matlab -batch "addpath ~/matlab/Github/Hippocampus; addpath ~/matlab/Github/Hippocampus/mt; addpath(genpath('~/matlab/Github/DPV')); addpath ~/matlab/Github/npy-matlab-master/npy-matlab; mtsess('auto','redo','save'); ori = pwd; cd strcat(ori,'/','cells'); mt_createCellsList; exit"
+matlab -batch "addpath ~/matlab/Github/Hippocampus; addpath ~/matlab/Github/Hippocampus/mt; addpath(genpath('~/matlab/Github/DPV')); addpath ~/matlab/Github/npy-matlab-master/npy-matlab; mtsess('auto','redo','save'); ori = pwd; cd(strcat(ori,'/','cells')); mt_createCellsList; exit;"
