ee3540057a846ed1a66a7b4a9e2fb4c7e8bcfb72
VirtualChaos <VirtualChaos@users.noreply.github.com>
2021-12-14 21:55:19 +0800

    Update test.sh

--- mt/test.sh
+++ mt/test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cmd1="matlab2019a -nojvm -nodisplay -nosplash -r \"addpath ~/matlab/Github/Hippocampus; addpath ~/matlab/Github/Hippocampus/mt; addpath(genpath('~/matlab/Github/DPV')); addpath ~/matlab/Github/npy-matlab-master/npy-matlab; mtsess('auto','redo','save'); cd cells; mt_createCellsList; exit\""
+var1="auto"
+var2="redo"
+var3="save"
+
+cmd1="matlab2019a -nojvm -nodisplay -nosplash -r \"addpath ~/matlab/Github/Hippocampus; addpath ~/matlab/Github/Hippocampus/mt; addpath ~/matlab/Github/DPV/miscellaneous; addpath ~/matlab/Github/npy-matlab-master/npy-matlab; mtsess($var1,$var2,$var3); cd cells; mt_createCellsList; exit\""
 eval $cmd1
