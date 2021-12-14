6d7337b4971e2d63fac84114e9737a4c30d85413
VirtualChaos <VirtualChaos@users.noreply.github.com>
2021-12-14 21:13:09 +0800

    update mt scripts

--- mt/mtcellHPC_control_file.sh
+++ mt/mtcellHPC_control_file.sh
@@ -4,9 +4,16 @@
 cmd1="cd cells"
 eval=$cmd1
 
-filename="cells_list.txt"
-n=1
-while read line; do
-echo "Line No. $n : $line"
-n=$(n+1))
-done
+# filename="cells_list.txt"
+# n=1
+# while read line; do
+# echo "Line No. $n : $line"
+# n=$(n+1))
+# done
+
+while read cell_no; do
+echo "$cell_no"
+cmd1="cd "
+eval $cmd1
+"qsub "
+done < "cells_list.txt"
