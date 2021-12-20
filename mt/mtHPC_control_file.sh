19d14628311becf3af17619f7e854cc4c1c73033
VirtualChaos <VirtualChaos@users.noreply.github.com>
2021-12-20 22:21:37 +0800

    Update mtHPC_control_file.sh

--- mt/mtHPC_control_file.sh
+++ mt/mtHPC_control_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 #Submit in session directory
-JID1=$(qsub mtsessHPC_submit_file.pbs)
+JID1=$(qsub ~/matlab/Github/Hippocampus/mt/mtsessHPC_submit_file.pbs)
 
-JID2=$(qsub -W depend=afterok:$JID1 mtcellHPC_control_file.sh)
+JID2=$(qsub -W depend=afterok:$JID1 ~/matlab/Github/Hippocampus/mt/mtcellHPC_control_file.sh)
