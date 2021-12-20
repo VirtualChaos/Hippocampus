15eb32e1e2d8294d7ee050bb6df329b0b4727487
VirtualChaos <VirtualChaos@users.noreply.github.com>
2021-12-20 22:19:58 +0800

    Update mtHPC_control_file.sh

--- mt/mtHPC_control_file.sh
+++ mt/mtHPC_control_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 #Submit in session directory
-JID1=$(qsub mtsessHPC_submit_file.sh)
+JID1=$(qsub mtsessHPC_submit_file.pbs)
 
 JID2=$(qsub -W depend=afterok:$JID1 mtcellHPC_control_file.sh)
